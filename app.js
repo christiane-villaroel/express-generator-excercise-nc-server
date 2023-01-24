2d7134e17c667ce89b9a9dd5b6a62263e0e10c96
Christiane Villaroel <christianevillaroel@gmail.com>
2023-01-24 12:01:11 -0500

    Passport

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const FileStore =require('session-file-store')(session);
 // after calling 'session-file-store' function
 // using require function, we are immediatly calling it 
 // in the (session). 
+const passport = require('passport');
+const authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -48,27 +50,23 @@ app.use(session({
     store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-    console.log(req.session);
+    console.log(req.user);
 
-    if (!req.session.user) {
-        const err = new Error('You are not authenticated!');
+    if (!req.user) {
+        const err = new Error('You are not authenticated!');                    
         err.status = 401;
         return next(err);
     } else {
-        if (req.session.user === 'authenticated') {
-            return next();
-        } else {
-            const err = new Error('You are not authenticated!');
-            err.status = 401;
-            return next(err);
-        }
+        return next();
     }
 }
-
 
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
