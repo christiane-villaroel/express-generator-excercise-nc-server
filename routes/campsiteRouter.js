4fe30df0d5d70bc145a405d894f3bbbe8f813df7
Christiane Villaroel <christianevillaroel@gmail.com>
2023-01-25 13:40:54 -0500

    Mongoose Population

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -5,15 +5,15 @@ const authenticate = require('../authenticate');
 const campsiteRouter = express.Router();
 
 campsiteRouter.route('/')
-
-.get((req, res,next) => {
+.get((req, res, next) => {
     Campsite.find()
-    .then(campsites=>{
+    .populate('comments.author')
+    .then(campsites => {
         res.statusCode = 200;
-        res.setHeader('Content-Type','application/json');
+        res.setHeader('Content-Type', 'application/json');
         res.json(campsites);
     })
-    .catch(err => next(err))
+    .catch(err => next(err));
 })
 .post(authenticate.verifyUser,(req, res,next) => {
     Campsite.create(req.body)
@@ -42,6 +42,7 @@ campsiteRouter.route('/')
 campsiteRouter.route('/:campsiteId')
 .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+    .populate('comments.author')
     .then(campsite => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
@@ -77,6 +78,7 @@ campsiteRouter.route('/:campsiteId')
 campsiteRouter.route('/:campsiteId/comments')
 .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+    .populate('comments.author')
     .then(campsite => {
         if (campsite) {
             res.statusCode = 200;
@@ -94,6 +96,7 @@ campsiteRouter.route('/:campsiteId/comments')
     Campsite.findById(req.params.campsiteId)
     .then(campsite => {
         if (campsite) {
+            req.body.author = req.user._id;
             campsite.comments.push(req.body);
             campsite.save()
             .then(campsite => {
@@ -140,6 +143,7 @@ campsiteRouter.route('/:campsiteId/comments')
 campsiteRouter.route('/:campsiteId/comments/:commentId')
 .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+    .populate('comments.author')
     .then(campsite => {
         if (campsite && campsite.comments.id(req.params.commentId)) {
             res.statusCode = 200;
